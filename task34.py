75612af419252c04c36cb932fcbef9633a72f8a8
ValeryBurlakov <wogl@mail.ru>
2023-02-21 23:06:19 +0300

    edited: task34.py, task36.py

--- task34.py
+++ task34.py
@@ -1,85 +1,46 @@
 strong_string = ('пара-ра-рам рам-пам-папам па-ра-па-дам').split()
-
-x = len(strong_string)
-
-f = list()
-for i in strong_string:
-    count = 0
-    for j in i:
-        if j == 'а':
-            count += 1
-    # print(count)
-    f.append(count)
-print(f)
-
-d = f[0]
-tount = 0
-
-for i in f:
-    if i == d:
-        tount += 1
-if tount == len(f):
-    print("same")
-else:
-    print("none")
-
-
-    
-fount = 0
-def winnie_the_pooh(strong_string):
-    
-    list_count_string = [(j) for i in strong_string for j in i if j == "а"]
-    list_count = [fount + 1 for i in list_count_string]
-    return j, list_count
-
-print(winnie_the_pooh(strong_string))
-
-
-
-
-
-
-strong_string = ('пара-ра-рам рам-пам-папам па-ра-па-дам').split()
 # strong_string = input("Введите ваше произведение искусства: ").lower().split()
+
 vowels = ['а', 'о', 'у', 'э', 'ы', 'я', 'ё', 'е', 'ю', 'и']
 answer = ["Парам пам-пам", "Пам парам"]
+
 # сокращенное решение
-fes = list()
+res = list()
 def winnie_the_pooh(strong_string):
-    fes = [sum(j in vowels for j in i) for i in strong_string]
-    return set(fes)
+    res = [sum(j in vowels for j in i) for i in strong_string]
+    return set(res)
 
 if len(winnie_the_pooh(strong_string)) == 1:
-     print('Парам пам-пам')
+     print(answer[0])
 else:
-     print("Пам парам")
+     print(answer[1])
 
 
 
-# простое решение
+# 2 решение
 # f = list()
 # for i in strong_string:
 #     count = 0
 #     for j in i:
-#         if j == 'а':
+#         if j in vowels:
 #             count += 1
-#     # print(count)
+
 #     f.append(count)
-# print(f)
 
 # d = f[0]
-# count_2 = 0
+# tount = 0
 
 # for i in f:
 #     if i == d:
-#         count_2 += 1
-# if count_2 == len(f):
+#         tount += 1
+# if tount == len(f):
 #     print("Парам пам-пам")
 # else:
 #     print("Пам парам")
 
-# strong_string = input("Введите ваше произведение искусства: ").lower().split() # пара-ра-рам рам-пам-папам па-ра-па-дам
 
+
+# 3 решение
 # res = list()
 # for i in strong_string:
 #     count = 0
@@ -87,7 +48,7 @@ else:
 #         if j in vowels:
 #             count += 1
 #     res.append(count)
-# print(len(set(res)))
+# # print(len(set(res)))
 # if len(set(res)) == 1:
 #     print('Парам пам-пам')
 # else:
--- task36.py
+++ task36.py
@@ -1,42 +1,41 @@
-from datetime import datetime
-import time
+# Без записи в таблицу
+def print_operation_table(operation, num_rows=6, num_columns=6):   
+    # for i in range(1, num_rows + 1): (print('\t'.join(str(operation(i, j))for j in range(1, num_columns + 1))))
+    for i in range(1, num_rows + 1):
+        print ('\t'.join(str(operation(i, j)) for j in range(1, num_columns + 1)))
 
-time_01 = datetime.now()
-def print_operation_table_3(operation, num_rows=100, num_columns=100000):   
-    for i in range(1, num_rows + 1): (print('\t'.join(str(operation(i, j))for j in range(1, num_columns + 1))))
-    # for i in range(1, num_rows + 1):
-    #     print ('\t'.join(str(operation(i, j)) for j in range(1, num_columns + 1)))
-print_operation_table_3(lambda x, y: x * y) 
-time_1 = datetime.now() - time_01
+print_operation_table(lambda x, y: x * y) 
 
 
-time_02 = datetime.now()
-def print_operation_table_2(operation, num_rows=100, num_columns=100000):
-    for x in range(1, num_rows + 1):
-        nums = []
-        for y in range(1, num_columns + 1):
-            num = operation(x, y)
-            nums.append(num)
-        print('\t'.join([str(x) for x in nums]))
-print_operation_table_2(lambda x, y: x * y) 
-time_2 = datetime.now() - time_02
 
+
+# С записью в таблицу
+# def print_operation_table_2(operation, num_rows=6, num_columns=6):
+#     for x in range(1, num_rows + 1):
+#         nums = []
+#         for y in range(1, num_columns + 1):
+#             num = operation(x, y)
+#             nums.append(num)
+#         print('\t'.join([str(x) for x in nums]))
+        
+# print_operation_table_2(lambda x, y: x * y) 
+
+
+
+
+# Черновик
 # num_rows = int() # ЧЕРНОВИК
 # num_columns = int() # ЧЕРНОВИК
 
 # def operation(x,y): # ЧЕРНОВИК
 #     return lambda x, y: x * y
 
-time_03 = datetime.now()
-def print_operation_table(operation, num_rows=100, num_columns=100000):
-    for rows_index in range(1, num_rows + 1):
-        for columns_index in range(1, num_columns + 1):
-            print(operation(rows_index, columns_index), end="\t")
-        print()
-print_operation_table(lambda x, y: x * y)
-time_3 = datetime.now() - time_03
+# def print_operation_table_3(operation, num_rows=6, num_columns=6):
+#     for rows_index in range(1, num_rows + 1):
+#         for columns_index in range(1, num_columns + 1):
+#             print(operation(rows_index, columns_index), end="\t")
+#         print()
 
-# print_operation_table(operation(num_rows, num_columns)) # ЧЕРНОВИК
-print(time_1)
-print(time_2)
-print(time_3)
+# print_operation_table_3(lambda x, y: x * y)
+
+# print_operation_table_3(operation(num_rows, num_columns)) # ЧЕРНОВИК
